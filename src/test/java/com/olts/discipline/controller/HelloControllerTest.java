23deeb3bc90483e67798eeb1e712ce23c5e1f083
TcoiOleg <tsoyolv@gmail.com>
2017-04-23 18:37:14 +0300

    1

---
+++ src/test/java/com/olts/discipline/controller/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package com.olts.discipline.controller;
+
+import org.junit.Test;
+
+/**
+ * OLTS on 23.04.2017.
+ */
+public class HelloControllerTest {
+    @Test
+    public void printWelcome() throws Exception {
+
+    }
+
+}
