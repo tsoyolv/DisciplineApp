b9a43f20b9eb32f425003cfeea744f368476ad79
TcoiOleg <tsoyolv@gmail.com>
2017-05-17 21:58:12 +0300

    jsp to thymleaf part 3. css and etc works! need to fix injection

--- src/main/java/com/olts/discipline/configuration/WebSecurityConfig.java
+++ src/main/java/com/olts/discipline/configuration/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/resources/**", "/registration").permitAll()
+                    .antMatchers("/registration", "/css/**", "/js/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
