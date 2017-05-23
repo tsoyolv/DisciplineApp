de210ce2b3185d3c4a9a955e50862daa34003346
TcoiOleg <tsoyolv@gmail.com>
2017-05-23 22:47:00 +0300

    https://github.com/TcoiOleg/DisciplineApp/issues/21 PART 1. react doesn't work

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -13,9 +13,71 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        client({method: 'GET', path: '/api/users'}).done(response => {
-            this.setState({users: response.entity._embedded.users});
+        this.loadFromServer(this.state.pageSize);
+    }
+
+    loadFromServer(pageSize) {
+        follow(client, root, [
+            {rel: 'users', params: {size: pageSize}}]
+        ).then(userCollection => {
+            return client({
+                method: 'GET',
+                path: userCollection.entity._links.profile.href,
+                headers: {'Accept': 'application/schema+json'}
+            }).then(schema => {
+                this.schema = schema.entity;
+                return userCollection;
+            });
+        }).done(userCollection => {
+            this.setState({
+                users: userCollection.entity._embedded.users,
+                attributes: Object.keys(this.schema.properties),
+                pageSize: pageSize,
+                links: userCollection.entity._links});
         });
+    }
+
+    onCreate(newUser) {
+        follow(client, root, ['users']).then(userCollection => {
+            return client({
+                method: 'POST',
+                path: userCollection.entity._links.self.href,
+                entity: newUser,
+                headers: {'Content-Type': 'application/json'}
+            })
+        }).then(response => {
+            return follow(client, root, [
+                {rel: 'users', params: {'size': this.state.pageSize}}]);
+        }).done(response => {
+            if (typeof response.entity._links.last != "undefined") {
+                this.onNavigate(response.entity._links.last.href);
+            } else {
+                this.onNavigate(response.entity._links.self.href);
+            }
+        });
+    }
+
+    onNavigate(navUri) {
+        client({method: 'GET', path: navUri}).done(userCollection => {
+            this.setState({
+                users: userCollection.entity._embedded.users,
+                attributes: this.state.attributes,
+                pageSize: this.state.pageSize,
+                links: userCollection.entity._links
+            });
+        });
+    }
+
+    onDelete(employee) {
+        client({method: 'DELETE', path: employee._links.self.href}).done(response => {
+            this.loadFromServer(this.state.pageSize);
+        });
+    }
+
+    updatePageSize(pageSize) {
+        if (pageSize !== this.state.pageSize) {
+            this.loadFromServer(pageSize);
+        }
     }
 
     render() {
@@ -28,31 +90,148 @@ class App extends React.Component {
 class UserList extends React.Component {
     render() {
         var users = this.props.users.map(user =>
-            <User key={user._links.self.href} user={user}/>
+            <User key={user._links.self.href} employee={user} onDelete={this.props.onDelete}/>
         );
+
+        var navLinks = [];
+        if ("first" in this.props.links) {
+            navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+        }
+        if ("prev" in this.props.links) {
+            navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
+        }
+        if ("next" in this.props.links) {
+            navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
+        }
+        if ("last" in this.props.links) {
+            navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
+        }
+
         return (
-            <table class="table">
-                <tbody>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Second Name</th>
-                </tr>
-                {users}
-                </tbody>
-            </table>
+            <div>
+                <input ref="pageSize" defaultValue={this.props.pageSize} onInput={this.handleInput}/>
+                <table>
+                    <tbody>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Second Name</th>
+                        <th></th>
+                    </tr>
+                    {users}
+                    </tbody>
+                </table>
+                <div>
+                    {navLinks}
+                </div>
+            </div>
         )
+    }
+
+    handleInput(e) {
+        e.preventDefault();
+        var pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
+        if (/^[0-9]+$/.test(pageSize)) {
+            this.props.updatePageSize(pageSize);
+        } else {
+            ReactDOM.findDOMNode(this.refs.pageSize).value =
+                pageSize.substring(0, pageSize.length - 1);
+        }
+    }
+
+    handleNavFirst(e){
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.first.href);
+    }
+
+    handleNavPrev(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.prev.href);
+    }
+
+    handleNavNext(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.next.href);
+    }
+
+    handleNavLast(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.last.href);
     }
 }
 
 class User extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete() {
+        this.props.onDelete(this.props.user);
+    }
+
     render() {
         return (
             <tr>
                 <td>{this.props.user.firstName}</td>
                 <td>{this.props.user.lastName}</td>
                 <td>{this.props.user.secondName}</td>
+                <td>
+                    <button onClick={this.handleDelete}>Delete</button>
+                </td>
             </tr>
+        )
+    }
+}
+
+class CreateDialog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        var newUser = {};
+        this.props.attributes.forEach(attribute => {
+            newUser[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+        });
+        this.props.onCreate(newUser);
+
+        // clear out the dialog's inputs
+        this.props.attributes.forEach(attribute => {
+            ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+        });
+
+        // Navigate away from the dialog to hide it.
+        window.location = "#";
+    }
+
+    render() {
+        var inputs = this.props.attributes.map(attribute =>
+            <p key={attribute}>
+                <input type="text" placeholder={attribute} ref={attribute} className="field" />
+            </p>
+        );
+
+        return (
+            <div>
+                <a href="#createUser">Create</a>
+
+                <div id="createUser" className="modalDialog">
+                    <div>
+                        <a href="#" title="Close" className="close">X</a>
+
+                        <h2>Create new user</h2>
+
+                        <form>
+                            {inputs}
+                            <button onClick={this.handleSubmit}>Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
