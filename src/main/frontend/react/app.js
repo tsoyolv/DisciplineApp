7e5f23b9f91f7727c4f0e3398df8604e9706a82e
TcoiOleg <tsoyolv@gmail.com>
2017-07-16 23:09:03 +0300

    lifting state up example. composition example. props.children example. done https://github.com/TcoiOleg/DisciplineApp/issues/31

--- src/main/frontend/react/app.js
+++ src/main/frontend/react/app.js
@@ -508,6 +508,8 @@ class Clock extends React.Component {
 
     /* Thanks to the setState() call, React knows the state has changed, and calls render() method again to learn
     what should be on the screen. */
+    /*  state is often called local or encapsulated. It is not accessible to any component other than the one that owns
+        and sets it. */
     tick() {
         this.setState({
             date: new Date()
@@ -723,17 +725,192 @@ class Reservation extends React.Component {
     }
 }
 
+/* Lifting state up start */
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>;
+    }
+    return <p>The water would not boil.</p>;
+}
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    /* React calls the function specified as onChange on the DOM <input> */
+    /* Its props, including onTemperatureChange, were provided by its parent component, the Calculator */
+    /* lift this state up to Calculator  <input> onChange */
+    handleChange(e) {
+        this.props.onTemperatureChange(e.target.value);
+    }
+
+    render() {
+        const temperature = this.props.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input value={temperature}
+                       onChange={this.handleChange} />
+            </fieldset>
+        );
+    }
+}
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+        this.state = {temperature: '', scale: 'c'};
+    }
+
+    /* the Calculator component asks React to re-render itself by calling this.setState() */
+    handleCelsiusChange(temperature) {
+        this.setState({scale: 'c', temperature});
+    }
+
+    handleFahrenheitChange(temperature) {
+        this.setState({scale: 'f', temperature});
+    }
+
+    /* React calls the Calculator component's render method to learn what the UI should look like. The values of both
+       inputs are recomputed based on the current temperature and the active scale. The temperature conversion is
+       performed here. */
+    /* React calls the render methods of the individual TemperatureInput components with their new props specified by
+       the Calculator. It learns what their UI should look like. */
+    render() {
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+        return (
+            <div>
+                <TemperatureInput
+                    scale="c"
+                    temperature={celsius}
+                    onTemperatureChange={this.handleCelsiusChange} />
+                <TemperatureInput
+                    scale="f"
+                    temperature={fahrenheit}
+                    onTemperatureChange={this.handleFahrenheitChange} />
+                <BoilingVerdict
+                    celsius={parseFloat(celsius)} />
+            </div>
+        );
+    }
+}
+/* Lifting stat up end */
+
+/* Composition start */
+
+function FancyBorder(props) {
+    /* without props.children it doesn't work */
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
+        </div>
+    );
+}
+
+function WelcomeDialog() {
+    return (
+        <FancyBorder color="blue">
+            <h3 className="Dialog-title">
+                Welcome
+            </h3>
+            <p className="Dialog-message">
+               This is composition with props.children.
+            </p>
+        </FancyBorder>
+    );
+}
+
+
+function Contacts() {
+    return <p>contacts</p>;
+}
+
+function Chat() {
+    return <p>chat</p>;
+}
+
+function SplitPane(props) {
+    return (
+        <div className="SplitPane">
+            <div className="SplitPane-left">
+                {props.left}
+            </div>
+            <div className="SplitPane-right">
+                {props.right}
+            </div>
+        </div>
+    );
+}
+
+function CompositionWithCustomChilds() {
+    return (
+        <SplitPane
+            left={
+                <Contacts />
+            }
+            right={
+                <Chat />
+            } />
+    );
+}
+
+/* Composition end */
+
 const numbers = [1, 2, 3];
 
 class Example extends React.Component {
     render() {
         return (
             <div>
+                <h2>State</h2>
                 <Toggle />
+                <h2>List</h2>
                 <NumberList numbers={numbers} />
+                <h2>Event handling</h2>
                 <NameForm/>
+                <h2>Select</h2>
                 <FlavorForm/>
+                <h2>Multiple inputs</h2>
                 <Reservation/>
+                <h2>Lifting state up</h2>
+                <Calculator/>
+                <h2>Composition</h2>
+                <WelcomeDialog/>
+                <h2>Composition with custom childs</h2>
+                <CompositionWithCustomChilds/>
+                <h2>Habits table</h2>
                 <App />
             </div>
         )
