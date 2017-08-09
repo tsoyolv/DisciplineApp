9cc9256d6c0f468107a0cb2c9d0879fe763c6673
o.tsoy <tsoyolv@gmail.com>
2017-08-09 16:02:18 +0300

    simple user out https://github.com/TcoiOleg/DisciplineApp/issues/38

--- src/main/frontend/react/components/user-component.js
+++ src/main/frontend/react/components/user-component.js
@@ -4,7 +4,20 @@ const React = require('react');
 
 class User extends React.Component {
     render() {
-        return <h1>User component</h1>;
+        return <div className="media-body">
+                    <hr/>
+                    <h3><strong>Bio</strong></h3>
+                    <p>Something interesting</p>
+                    <hr/>
+                    <h3><strong>Location</strong></h3>
+                    <p>Earth</p>
+                    <hr/>
+                    <h3><strong>Gender</strong></h3>
+                    <p>Male</p>
+                    <hr/>
+                    <h3><strong>Birthday</strong></h3>
+                    <p>January 1, 1601</p>
+                </div>;
     }
 }
 
