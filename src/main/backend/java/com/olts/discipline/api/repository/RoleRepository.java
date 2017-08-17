e6dcf8bc2dbc9bd8ea1367d7ce083b1c7a675620
TcoiOleg <tsoyolv@gmail.com>
2017-08-17 08:03:41 +0300

    back. exclude roles from rest https://github.com/TcoiOleg/DisciplineApp/issues/38

--- src/main/backend/java/com/olts/discipline/api/repository/RoleRepository.java
+++ src/main/backend/java/com/olts/discipline/api/repository/RoleRepository.java
@@ -2,10 +2,12 @@ package com.olts.discipline.api.repository;
 
 import com.olts.discipline.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * OLTS on 13.05.2017.
  */
+@RepositoryRestResource(exported = false)
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
 
--- src/main/backend/java/com/olts/discipline/model/User.java
+++ src/main/backend/java/com/olts/discipline/model/User.java
@@ -65,6 +65,7 @@ public class User implements Serializable {
     @OneToMany(mappedBy = "taskUser")
     private Set<Task> tasks = new HashSet<>();
 
+    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
