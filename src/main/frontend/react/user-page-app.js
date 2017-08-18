3d97a17c6aef3a3dcde723343053862e7efad383
TcoiOleg <tsoyolv@gmail.com>
2017-08-18 08:03:45 +0300

    frontend. sendRequest for GET method https://github.com/TcoiOleg/DisciplineApp/issues/38

--- src/main/frontend/react/user-page-app.js
+++ src/main/frontend/react/user-page-app.js
@@ -2,6 +2,9 @@
 
 const React = require('react');
 const client = require('./modules/client');
+
+const GET_USER_PATH = '/api/users';
+
 import User from './components/user-component'
 
 export default class UserApp extends React.Component {
@@ -12,18 +15,31 @@ export default class UserApp extends React.Component {
     }
 
     componentDidMount() {
+        this.setCurrentUserToState();
+    }
+
+    setCurrentUserToState () {
+        this.sendRequest('GET', GET_USER_PATH, function (response) {
+            this.setState({user: response.entity});
+        });
+    }
+
+    sendRequest(method, path, callback) {
         client({
-            method: 'GET',
-            path: 'http://localhost:8888/api/users/1',
+            method: method,
+            path: path,
             headers: {
                 'X-CSRF-TOKEN': $("meta[name='_csrf']").attr("content")
             }
-        }).done(response =>
-            this.setState({user: user})
+        }).done(response => {
+            if (callback) { callback(response);}
+            }
         );
     }
 
     render() {
-        return <User/>;
+        var t = this.state.user;
+        var username = t.username;
+        return <User username={username}/>;
     }
 }
