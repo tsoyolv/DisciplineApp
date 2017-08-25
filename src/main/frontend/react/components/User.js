a7a8bd021a08c3413696f78a92a8605061a94d76
TcoiOleg <tsoyolv@gmail.com>
2017-08-25 08:00:07 +0300

    frontend. user view with level https://github.com/TcoiOleg/DisciplineApp/issues/38

--- src/main/frontend/react/components/User.js
+++ src/main/frontend/react/components/User.js
@@ -12,9 +12,9 @@ export default class User extends React.Component {
             <div>
                 <h1 className="page-header">{this.props.user.firstName} {this.props.user.secondName}</h1>
                 <div className="progress">
-                    <div className="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style={{width: '70%'}}></div>
+                    <div className="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style={{width: this.props.user.levelPercentage + '%'}}></div>
                 </div>
-                <span className="text-muted">Level </span>
+                <span className="text-muted">Level {this.props.user.level}</span>
                 <div className="row placeholders">
                     <div className="col-xs-6 col-sm-3 placeholder">
                         <img src="images/image.jpg" width="200" height="200" className="img-responsive"
@@ -22,22 +22,22 @@ export default class User extends React.Component {
                     </div>
                     <div className="col-xs-6 col-sm-3 placeholder">
                         <div>
-                            <span className="text-muted">User Name</span>
                             <h4>{this.props.user.username}</h4>
-                            <span className="text-muted">First Name</span>
+                            <span className="text-muted">User Name</span>
                             <h4>{this.props.user.firstName}</h4>
-                            <span className="text-muted">Second Name</span>
+                            <span className="text-muted">First Name</span>
                             <h4>{this.props.user.secondName}</h4>
-                            <span className="text-muted">Last Name</span>
+                            <span className="text-muted">Second Name</span>
                             <h4>{this.props.user.lastName}</h4>
-                            <span className="text-muted">Email</span>
+                            <span className="text-muted">Last Name</span>
                             <h4>{this.props.user.email}</h4>
-                            <span className="text-muted">All score</span>
+                            <span className="text-muted">Email</span>
                             <h4>{this.props.user.score}</h4>
-                            <span className="text-muted">Habit score</span>
+                            <span className="text-muted">All score</span>
                             <h4>{this.props.user.habitScore}</h4>
-                            <span className="text-muted">Task score</span>
+                            <span className="text-muted">Habit score</span>
                             <h4>{this.props.user.taskScore}</h4>
+                            <span className="text-muted">Task score</span>
                         </div>
                     </div>
                 </div>
