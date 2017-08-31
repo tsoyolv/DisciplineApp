ed78165a81c91da2463a941cff4f17f5afbcfc3d
TcoiOleg <tsoyolv@gmail.com>
2017-08-31 07:45:21 +0300

    backend. added websockets for user https://github.com/TcoiOleg/DisciplineApp/issues/38

--- src/main/backend/java/com/olts/discipline/rest/api/UserRestController.java
+++ src/main/backend/java/com/olts/discipline/rest/api/UserRestController.java
@@ -1,11 +1,15 @@
 package com.olts.discipline.rest.api;
 
 import com.olts.discipline.api.service.UserService;
+import com.olts.discipline.entity.User;
 import com.olts.discipline.rest.dto.UserGETDto;
 import com.olts.discipline.rest.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,15 +26,18 @@ class UserRestController implements ApplicationEventPublisherAware {
     public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
         this.publisher = applicationEventPublisher;
     }
-
+    @Resource
+    EntityLinks entityLinks;
     @Resource
     private UserService userService;
     @Resource(name = "userMapper")
     private UserMapper userMapper;
 
     @GetMapping("/users/current")
-    private @ResponseBody
-    ResponseEntity<UserGETDto> getHabitsByCurrentUser() {
-        return ResponseEntity.ok(userMapper.userToUserGetDto(userService.getCurrent()));
+    private @ResponseBody ResponseEntity<UserGETDto> getCurrent() {
+        User current = userService.getCurrent();
+        UserGETDto userGETDto = userMapper.userToUserGetDto(current);
+        userGETDto.add(entityLinks.linkForSingleResource(User.class, current.getId()).withSelfRel());
+        return ResponseEntity.ok(userGETDto);
     }
 }
