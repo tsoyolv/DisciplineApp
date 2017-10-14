f09542b647b2bc446578ed6821cb4cc6fcc58bab
TcoiOleg <tsoyolv@gmail.com>
2017-10-14 17:49:14 +0300

    frontend. react router example https://github.com/TcoiOleg/DisciplineApp/issues/53

--- src/main/frontend/react/app.js
+++ src/main/frontend/react/app.js
@@ -14,7 +14,83 @@ import ChallengesPage from './ChallengesPage';
 import ChallengePage from './ChallengePage';
 import Summary from './Summary';
 
-/* todo react routing */
+/*import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
+
+import './css/scroll.css';
+
+/!* todo react routing *!/
+const BasicExample = () => (
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/topics">Topics</Link></li>
+            </ul>
+
+            <hr/>
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/topics" component={Topics}/>
+        </div>
+    </Router>
+)
+
+const Home = () => (
+    <div className="newss">
+        <h2>Home</h2>
+    </div>
+)
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+)
+
+const Topics = ({ match }) => (
+    <div>
+        <h2>Topics</h2>
+        <ul>
+            <li>
+                <Link to={`${match.url}/rendering`}>
+                    Rendering with React
+                </Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/components`}>
+                    Components
+                </Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/props-v-state`}>
+                    Props v. State
+                </Link>
+            </li>
+        </ul>
+
+        <Route path={`${match.url}/:topicId`} component={Topic}/>
+        <Route exact path={match.url} render={() => (
+            <h3>Please select a topic.</h3>
+        )}/>
+    </div>
+)
+
+const Topic = ({ match }) => (
+    <div>
+        <h3>{match.params.topicId}</h3>
+    </div>
+)
+
+ReactDOM.render(
+    <BasicExample />,
+    document.getElementById('user')
+);*/
 
 if(document.getElementById('all_habits')) {
     ReactDOM.render(
